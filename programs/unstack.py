5364858b88405593bc367f0e8a1a04928ba5d7cb
Martin Fixman <martinfixman@gmail.com>
2017-04-08 18:30:14 -0300

    Fixes to unstack.

--- programs/unstack.py
+++ programs/unstack.py
@@ -4,24 +4,24 @@ import logging
 import pandas
 import sys
 
-from argparse import Argumentparser
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s|\t%(message)s', level=logging.DEBUG, datefmt='%H:%M:%S')
     
     last = None
     cols = None
-    for e, chunk in enumerate(pandas.read_csv(sys.stdin, sep = '|', index_col = [0, 1], chunksize = 100000)):
+    for e, chunk in enumerate(pandas.read_csv(sys.stdin, sep = '|', index_col = [0, 1], chunksize = 1000000)):
         if e & (e - 1) == 0:
             logging.debug('Parsing chunk {}'.format(e))
 
-        if last:
+        if last is not None:
             chunk = pandas.concat([last, chunk])
             last = None
         
         if chunk.index[-1][1] == 1:
-            last = chunk[-1]
-            chunk = chunk[:-1]
+            last = chunk.iloc[[-1]]
+            chunk = chunk.iloc[:-1]
 
         trans = chunk.unstack(level = 1, fill_value = 0)
         trans.columns = ['{}_d{}'.format(x, y) for (x, y) in trans.columns]
@@ -33,8 +33,7 @@ if __name__ == '__main__':
 
         trans.to_csv(sys.stdout, sep = '|', index = True, header = e == 0)
 
-    
-    if last:
+    if last is not None:
         last = last.unstack(level = 1, fill_value = 0).reindex(columns = cols, fill_value = 0)
         last.to_csv(sys.stdout, sep = '|', index = True, header = False)
 
