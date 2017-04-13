f7780962a454e81898517df1c591909431ad3649
Martin Fixman <martinfixman@gmail.com>
2017-04-12 21:00:47 -0300

    Fixing problem with column name accumulation.

--- programs/partition_merge_accum.py
+++ programs/partition_merge_accum.py
@@ -89,7 +89,7 @@ if __name__ == '__main__':
 
         full = chunk.merge(extra, left_on = args.merge_col, right_index = True).drop(args.merge_col, axis = 1)
         accum = full.groupby([full.index] + args.columns).sum().unstack(level = range(1, len(args.columns) + 1), fill_value = 0)
-        accum.columns = ['_'.join('{}{}'.format('' if type(x) == str else 'd', x)) for x in accum.columns]
+        accum.columns = ['_'.join('{}{}'.format('' if type(x) == str else 'd', x) for y in x) for x in accum.columns]
         accum.index.rename(extra.index.name, inplace = True)
 
         accum.to_csv(sys.stdout, sep = '|', index = True, header = e == 0)
